2386273a0a0612e2de3ad0e5ca4c8ca461f44798
Abolaji Ayeni <ayenibolaji07@gmail.com>
2024-03-02 16:29:10 +0100

    removed unused folders

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,30 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: false,
       ),
-      home: const Material(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ,
+        title: const Text('Buy my first car'),
+      ),
     );
   }
 }
