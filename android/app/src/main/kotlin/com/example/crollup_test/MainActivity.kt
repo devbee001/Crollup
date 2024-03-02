1e15f6d3b27a7706767c003c2273fb2d753e3288
Abolaji Ayeni <ayenibolaji07@gmail.com>
2024-03-02 16:24:59 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/crollup_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crollup_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: const Material(),
+    );
+  }
+}
